2fca0d13b74de87365e45ed60cf6e1d9e7d0a4fb
Arne Wiese <arne@wiese.me>
2020-04-10 20:09:11 +0200

    Throw error if env not found

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,15 +1,29 @@
 const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  env: {
-    HASURA_URL: process.env.HASURA_URL,
-    HASURA_ADMIN_SECRET: process.env.HASURA_ADMIN_SECRET,
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add the new plugin to the existing webpack plugins
-    config.plugins.push(new Dotenv({ silent: true }));
+module.exports = () => {
+  const { HASURA_URL, HASURA_ADMIN_SECRET } = process.env;
 
-    return config;
-  },
-  modules: true,
+  if (!HASURA_URL) {
+    throw new Error("HASURA_URL not found");
+  }
+
+  if (!HASURA_ADMIN_SECRET) {
+    throw new Error("HASURA_ADMIN_SECRET not found");
+  }
+
+  console.log(HASURA_URL, HASURA_ADMIN_SECRET);
+
+  return {
+    env: {
+      HASURA_URL,
+      HASURA_ADMIN_SECRET,
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // Add the new plugin to the existing webpack plugins
+      config.plugins.push(new Dotenv({ silent: true }));
+
+      return config;
+    },
+    modules: true,
+  };
 };

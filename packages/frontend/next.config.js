79e0ec61eabc80726956887942fbe2263f2294e9
Fabian <breytex@gmail.com>
2020-04-10 17:29:40 +0200

    Added graphql hooks

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,4 +1,11 @@
+const Dotenv = require("dotenv-webpack");
+
 module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the new plugin to the existing webpack plugins
+    config.plugins.push(new Dotenv({ silent: true }));
+
+    return config;
+  },
   modules: true,
-  env: {},
 };
--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,16 +1,29 @@
+const fetch = require("isomorphic-unfetch");
+
 import * as React from "react";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import { customTheme } from "../chakra/theme";
-import "../styles.css";
 import PageLayout from "../components/PageLayout";
+import { GraphQLClient, ClientContext } from "graphql-hooks";
+import "../styles.css";
+console.log(process.env.HASURA_URL);
+
+const client = new GraphQLClient({
+  url: process.env.HASURA_URL,
+  headers: {
+    "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
+  },
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CSSReset />
-      <PageLayout>
-        <Component {...pageProps} />
-      </PageLayout>
-    </ThemeProvider>
+    <ClientContext.Provider value={client}>
+      <ThemeProvider theme={customTheme}>
+        <CSSReset />
+        <PageLayout>
+          <Component {...pageProps} />
+        </PageLayout>
+      </ThemeProvider>
+    </ClientContext.Provider>
   );
 }

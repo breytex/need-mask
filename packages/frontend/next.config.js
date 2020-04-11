b8ddb635eb9a7f38f83fcc4b1614ed6b49b01831
Fabian <breytex@gmail.com>
2020-04-11 18:17:50 +0200

    Fixed env variable drill

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -4,6 +4,10 @@ module.exports = {
   env: {
     HASURA_URL: process.env.HASURA_URL,
     HASURA_ADMIN_SECRET: process.env.HASURA_ADMIN_SECRET,
+    SPACES_KEY: process.env.SPACES_KEY,
+    SPACES_SECRET: process.env.SPACES_SECRET,
+    ALGOLIA_PLACES_APPID: process.env.ALGOLIA_PLACES_APPID,
+    ALGOLIA_PLACES_KEY: process.env.ALGOLIA_PLACES_KEY,
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Add the new plugin to the existing webpack plugins

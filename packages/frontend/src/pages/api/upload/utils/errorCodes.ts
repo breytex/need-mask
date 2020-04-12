21789939fb6d1f97d475a9fd0d664c7a80356c44
Firat Özcan <firat.oezcan@gmail.com>
2020-04-12 16:48:48 +0200

    Errorcode management

--- packages/frontend/src/pages/api/upload/utils/errorCodes.ts
+++ packages/frontend/src/pages/api/upload/utils/errorCodes.ts
@@ -1,13 +1,22 @@
-type ERROR_CODES = {
-  NUDE_MATERIAL_DETECTED: "The file couldn't be uploaded because there is too much visible skin",
-  UNSUPPORTED_FILE_TYPE: "The file couldn't be uploaded because the filetype is not supported"
-  SIZE_EXCEEDED: "The file is too large"
-}
-const ERROR_CODES = {
-  NUDE_MATERIAL_DETECTED: "The file couldn't be uploaded because there is too much visible skin",
-  UNSUPPORTED_FILE_TYPE: "The file couldn't be uploaded because the filetype is not supported",
-  SIZE_EXCEEDED: "The file is too large"
+const createErrors = <T extends { [key: string]: string }, U = keyof T>(codes: T) => (key: U) => {
+  function t(_key: string) {
+    if (!codes[_key] && codes[_key] !== '') {
+      return { errors: [{ name, message: _key }] };
+    }
+
+    return { errors: [{ name, message: codes[_key] }] };
+  }
+  return t(key as unknown as string)
 }
 
-const createUploadError = (name: keyof ERROR_CODES) => ({ errors: [{ name, message: ERROR_CODES[name] }] })
+const ERROR_CODES = {
+  UPLOAD: (() => createErrors({
+    NUDE_MATERIAL_DETECTED: "The file couldn't be uploaded because there is too much visible skin",
+    UNSUPPORTED_FILE_TYPE: "The file couldn't be uploaded because the filetype is not supported",
+    SIZE_EXCEEDED: "The file is too large"
+  }))()
+}
+
+const createUploadError = ERROR_CODES.UPLOAD
+
 export { ERROR_CODES, createUploadError }

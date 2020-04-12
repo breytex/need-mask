1d70d89ef844e62dc7e8d54c338075209b6d0883
Firat Özcan <firat.oezcan@gmail.com>
2020-04-13 01:53:10 +0200

    Uploads are now public and small refactors

--- packages/frontend/src/pages/api/spaces/upload/index.ts
+++ packages/frontend/src/pages/api/spaces/upload/index.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import MultiParty from "multiparty";
 import { handleFile } from "./handleFile";
 import { randomBytes } from "crypto";
-import { s3 } from '../../utils/s3'
+import { s3, bucketName, tempFolder } from '../../utils/s3'
 export const config = {
   api: {
     bodyParser: false,
@@ -16,19 +16,24 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
   form.on("part", async function (part) {
     if (part.filename) {
+      /**
+       * Catches multiple files in an upload. 
+       * Sadly we cannot send an error to the user because we already sent them an response
+       */
+      if (res.headersSent) return
+
       const { errors, data, mimeType } = await handleFile(part);
       if (errors) {
         return res.status(403).send(errors);
       }
       const hashedFilename = `${randomBytes(4).toString("hex")}--${part.filename}`;
-      const params = {
-        Bucket: "need-mask",
-        Key: `definitelyNotTemp/${hashedFilename}`,
+      await s3.upload({
+        Bucket: bucketName,
+        Key: `${tempFolder}/${hashedFilename}`,
         ContentType: mimeType,
         Body: data,
-        acl: "public-read",
-      };
-      await s3.upload(params).promise();
+        ACL: "public-read",
+      }).promise();
       res.send({ fileName: hashedFilename });
     }
   });

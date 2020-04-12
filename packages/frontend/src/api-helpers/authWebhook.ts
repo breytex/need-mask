e7d840df931defc3e1a197b4d8eaa5363b6d3d95
Fabian <breytex@gmail.com>
2020-04-12 14:55:24 +0200

    removed console.log

--- packages/frontend/src/api-helpers/authWebhook.ts
+++ packages/frontend/src/api-helpers/authWebhook.ts
@@ -5,7 +5,6 @@ export const authWebhook = (next) => (
   req: WebhookRequest<any>,
   res: NextApiResponse
 ) => {
-  console.log({ test: req.headers });
   if (req.headers.webhooksecret !== process.env.WEBHOOK_AUTH_SECRET) {
     res.statusCode = 500;
     res.setHeader("Content-Type", "application/json");

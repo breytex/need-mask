72f0e7671aa1eb4e2cae1f2e69e6049d39b89df2
Fabian <breytex@gmail.com>
2020-04-15 23:13:51 +0200

    Fixed formating

--- packages/frontend/src/helpers/functions.ts
+++ packages/frontend/src/helpers/functions.ts
@@ -1,10 +1,17 @@
-type ReduceType<T> = (previousValue: T, currentValue: T, currentIndex?: number, array?: T[]) => any
+type ReduceType<T> = (
+  previousValue: T,
+  currentValue: T,
+  currentIndex?: number,
+  array?: T[]
+) => any;
 
-export function propertyReducer<T extends Array<{ [key: string]: any }>>(arr: T) {
+export function propertyReducer<T extends Array<{ [key: string]: any }>>(
+  arr: T
+) {
   return (property: keyof T[0] & string, callback: ReduceType<T[0]>) =>
     arr.map((t) => t[property]).reduce(callback);
 }
 
 export function toPrice(cents: number) {
-  return (cents / 100).toFixed(2) + '€'
+  return (cents / 100).toFixed(2) + "€";
 }

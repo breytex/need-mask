9670507c8bc2e7b855c4b4eb05f5f3d93a386620
Fabian <breytex@gmail.com>
2020-04-16 19:59:50 +0200

    Changed feedback endpoint to accept all state transitions to "feedback"

--- packages/frontend/src/pages/api/supplier/feedback.ts
+++ packages/frontend/src/pages/api/supplier/feedback.ts
@@ -5,7 +5,7 @@ import { sendMail, SendMailParams } from "../utils/sendMail";
 
 export default createWebhooookHandler<Supplier>(async (req, res) => {
   const { data } = req.body.event;
-  if (data.old.status !== "pending" || data.new.status !== "feedback") {
+  if (data.new.status !== "feedback") {
     return res.end(
       "Row's status was not switched from pending to feedback; this is a no-op."
     );

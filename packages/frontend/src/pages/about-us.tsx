335c62de2618af3688455b558b8e35ca01e26ceb
Arne Wiese <arne@wiese.me>
2020-04-19 11:38:39 +0200

    Update team and imprint

--- packages/frontend/src/pages/about-us.tsx
+++ packages/frontend/src/pages/about-us.tsx
@@ -1,23 +1,121 @@
 import * as React from "react";
 import SiteHero from "../components/SiteHero";
 import { Heading, Text } from "@chakra-ui/core";
+import { Box, Link } from "@chakra-ui/core/dist";
+import SimpleGrid from "@chakra-ui/core/dist/SimpleGrid";
+import { NextPage } from "next";
+import Card from "../components/chakra/Card";
 
-const AboutUs = () => {
-  let div = (
+type Member = {
+  title: string;
+  description: string;
+  role: string;
+  linkedIn: string;
+  imageSrc: string;
+};
+
+const AboutUs: NextPage<{ team: Member[] }> = ({ team }) => {
+  return (
     <>
-      <div>
-        <SiteHero
-          title="About Us"
-          description="A non-profit registry for sourcing protective masks in Europe"
-        />
-        <Heading size="md">Coordinated by</Heading>
-        <Text>Dr.-Ing. David Schmelzeisen</Text>
-        <Text>Jan Wilmking</Text>
-        <Text>Jürgen Brocker</Text>
-      </div>
+      <SiteHero
+        title="About Us"
+        description="A non-profit registry for sourcing protective masks in Europe"
+      />
+
+      <Box maxWidth="800px" mx="auto" textAlign="center">
+        <Box mb="16">
+          <SimpleGrid columns={3} spacing={2}>
+            {team.map((member) => (
+              <Card key={member.title} textAlign="center" p="4">
+                <Heading as="h4" size="sm">
+                  {member.title}
+                </Heading>
+                <Text fontSize="sm" color="#495057" mb="2">
+                  {member.role}
+                </Text>
+                <Box textAlign="center">
+                  <img
+                    src={`${member.imageSrc}.jpg`}
+                    srcSet={`${member.imageSrc}@2x.jpg 2x`}
+                    alt={member.title}
+                    height="180"
+                    width="180"
+                    style={{ borderRadius: "50%", display: "inline-block" }}
+                  />
+                </Box>
+                <Link fontSize="xs" href={member.linkedIn} target="_blank">
+                  LinkedIn
+                </Link>
+              </Card>
+            ))}
+          </SimpleGrid>
+        </Box>
+
+        <Box>
+          <Heading as="h2" size="md" mb="4">
+            Advisor
+          </Heading>
+          <Text mb="2">Jan Wilmking</Text>
+        </Box>
+      </Box>
     </>
   );
-  return div;
 };
+
+export async function getServerSideProps() {
+  const team = [
+    {
+      title: "Dr.-Ing. David Schmelzeisen",
+      description: "",
+      role: "Project Lead",
+      linkedIn: "https://www.linkedin.com/in/david-schmelzeisen/",
+      imageSrc: "/images/team/ds",
+    },
+    {
+      title: "Arne Wiese",
+      description: "",
+      role: "Software Engineer",
+      linkedIn: "https://www.linkedin.com/in/arnewiese/",
+      imageSrc: "/images/team/aw",
+    },
+    {
+      title: "Fabian Schulze",
+      description: "",
+      role: "Software Engineer",
+      linkedIn: "https://www.linkedin.com/in/fabian-schulze-code-consulting/",
+      imageSrc: "/images/team/fs",
+    },
+    {
+      title: "Firat Özcan",
+      description: "",
+      role: "Software Engineer",
+      linkedIn: "https://www.linkedin.com/in/firatoezcan",
+      imageSrc: "/images/team/fo",
+    },
+    {
+      title: "Kira Hirschberger",
+      description: "",
+      role: "Project Assistant",
+      linkedIn: "https://www.linkedin.com/in/firatoezcan",
+      imageSrc: "/images/team/ds",
+    },
+    {
+      title: "Hanna Hohenbild",
+      description: "",
+      role: "Project Assistant",
+      linkedIn: "https://www.linkedin.com/in/firatoezcan",
+      imageSrc: "/images/team/ds",
+    },
+  ];
+
+  return {
+    props: {
+      team: [
+        team[0],
+        ...team.slice(1, team.length).sort(() => Math.random() - 0.5),
+      ],
+    },
+  };
+}
 
 export default AboutUs;
--- packages/frontend/src/pages/imprint.tsx
+++ packages/frontend/src/pages/imprint.tsx
@@ -1,59 +1,75 @@
 import * as React from "react";
 import { Heading, Text } from "@chakra-ui/core";
 import SiteHero from "../components/SiteHero";
+import { Box, List, ListItem } from "@chakra-ui/core/dist";
 
 const Imprint = () => {
   return (
-    <div>
+    <>
       <SiteHero title="Imprint" />
+      <Box maxWidth="600px" mx="auto">
+        <Box mb="4">
+          <Heading as="h4" size="sm">
+            Legal Disclosure
+          </Heading>
+          <Box>
+            Information in accordance with Section 5 TMG <br />
+            David Schmelzeisen <br />
+            Kuckhoffstr. 31 52064 Aachen <br />
+            <Heading size="xs" mt="2">
+              Contact Information
+            </Heading>
+            E-Mail: david.schmelzeisen@ita.rwth-aachen.de <br />
+            Internet address: www.need-mask.com <br />
+            <Heading size="xs" mt="2">
+              Graphics and Image Sources
+            </Heading>
+            <List styleType="disc">
+              <ListItem>https://unsplash.com/license</ListItem>
+              <li>https://undraw.co/license</li>
+            </List>
+          </Box>
+        </Box>
 
-      <Heading as="h4" size="sm">
-        Legal Disclosure
-      </Heading>
-      <Text mb={4}>
-        Information in accordance with Section 5 TMG David Schmelzeisen
-        Kuckhoffstr. 31 52064 Aachen Contact Information E-Mail:
-        david.schmelzeisen@ita.rwth-aachen.de Internet address:
-        www.need-mask.com Graphics and Image Sources
-        https://unsplash.com/license
-      </Text>
-      <Heading as="h4" size="sm">
-        Disclaimer
-      </Heading>
-      <Text mb={4}>
-        Accountability for content The contents of our pages have been created
-        with the utmost care. However, we cannot guarantee the contents'
-        accuracy, completeness or topicality. According to statutory provisions,
-        we are furthermore responsible for our own content on these web pages.
-        In this matter, please note that we are not obliged to monitor the
-        transmitted or saved information of third parties, or investigate
-        circumstances pointing to illegal activity. Our obligations to remove or
-        block the use of information under generally applicable laws remain
-        unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).
-      </Text>
-      <Heading as="h4" size="sm">
-        Accountability
-      </Heading>
-      <Text mb={4}>
-        Accountability for links Responsibility for the content of external
-        links (to web pages of third parties) lies solely with the operators of
-        the linked pages. No violations were evident to us at the time of
-        linking. Should any legal infringement become known to us, we will
-        remove the respective link immediately.
-      </Text>
-      <Heading as="h4" size="sm">
-        Copyright
-      </Heading>
-      <Text>
-        Our web pages and their contents are subject to German copyright law.
-        Unless expressly permitted by law, every form of utilizing, reproducing
-        or processing works subject to copyright protection on our web pages
-        requires the prior consent of the respective owner of the rights.
-        Individual reproductions of a work are only allowed for private use. The
-        materials from these pages are copyrighted and any unauthorized use may
-        violate copyright laws.
-      </Text>
-    </div>
+        <Heading as="h4" size="sm">
+          Disclaimer
+        </Heading>
+        <Text mb={4}>
+          Accountability for content The contents of our pages have been created
+          with the utmost care. However, we cannot guarantee the contents'
+          accuracy, completeness or topicality. According to statutory
+          provisions, we are furthermore responsible for our own content on
+          these web pages. In this matter, please note that we are not obliged
+          to monitor the transmitted or saved information of third parties, or
+          investigate circumstances pointing to illegal activity. Our
+          obligations to remove or block the use of information under generally
+          applicable laws remain unaffected by this as per §§ 8 to 10 of the
+          Telemedia Act (TMG).
+        </Text>
+        <Heading as="h4" size="sm">
+          Accountability
+        </Heading>
+        <Text mb={4}>
+          Accountability for links Responsibility for the content of external
+          links (to web pages of third parties) lies solely with the operators
+          of the linked pages. No violations were evident to us at the time of
+          linking. Should any legal infringement become known to us, we will
+          remove the respective link immediately.
+        </Text>
+        <Heading as="h4" size="sm">
+          Copyright
+        </Heading>
+        <Text>
+          Our web pages and their contents are subject to German copyright law.
+          Unless expressly permitted by law, every form of utilizing,
+          reproducing or processing works subject to copyright protection on our
+          web pages requires the prior consent of the respective owner of the
+          rights. Individual reproductions of a work are only allowed for
+          private use. The materials from these pages are copyrighted and any
+          unauthorized use may violate copyright laws.
+        </Text>
+      </Box>
+    </>
   );
 };
 

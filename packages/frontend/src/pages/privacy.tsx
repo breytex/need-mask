494bbe69890cbe9579aca8e458ae556cf3261895
Arne Wiese <arne@wiese.me>
2020-04-19 20:37:27 +0200

    Add privacy page

--- packages/frontend/src/pages/privacy.tsx
+++ packages/frontend/src/pages/privacy.tsx
@@ -1,18 +1,258 @@
 import * as React from "react";
 import { Heading, Text } from "@chakra-ui/core";
 import SiteHero from "../components/SiteHero";
-import { Box } from "@chakra-ui/core/dist";
+import { Box, List, ListItem } from "@chakra-ui/core/dist";
 import { NextPage } from "next";
 
 const Privacy: NextPage = () => {
   return (
     <>
-      <SiteHero title="Privacy" />
+      <SiteHero title="Privacy Policy for need-mask.com" />
       <Box maxWidth="600px" mx="auto">
-        <Heading as="h4" size="sm">
-          some heading
+        <Text mb="2">
+          At NeedMask, accessible from http://www.need-mask.com/, one of our
+          main priorities is the privacy of our visitors. This Privacy Policy
+          document contains types of information that is collected and recorded
+          by NeedMask and how we use it.
+        </Text>
+        <Text mb="2">
+          If you have additional questions or require more information about our
+          Privacy Policy, do not hesitate to contact us.
+        </Text>
+        <Text mb="2">
+          This Privacy Policy applies only to our online activities and is valid
+          for visitors to our website with regards to the information that they
+          shared and/or collect in NeedMask. This policy is not applicable to
+          any information collected offline or via channels other than this
+          website.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          Consent
         </Heading>
-        <Text mb={4}>some text</Text>
+        <Text mb="2">
+          By using our website, you hereby consent to our Privacy Policy and
+          agree to its terms.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          Information we collect
+        </Heading>
+        <Text mb="2">
+          The personal information that you are asked to provide, and the
+          reasons why you are asked to provide it, will be made clear to you at
+          the point we ask you to provide your personal information. If you
+          contact us directly, we may receive additional information about you
+          such as your name, email address, phone number, address, the contents
+          of the message and/or attachments you may send us, and any other
+          information you may choose to provide. When you register for an
+          Account, we may ask for your contact information, including items such
+          as name, company name, address, email address, and telephone number.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          Contact form
+        </Heading>
+        <Text mb="2">
+          By filling in the contact form with their Data, the User authorizes
+          NeedMask to use these details to reply to requests for information,
+          quotes or any other kind of request as indicated by the form’s header.
+        </Text>
+        <Text mb="2">
+          Personal Data processed: address; city; company name; country; email
+          address; first name; last name; VAT Number; website; ZIP/Postal code.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          How we use your information
+        </Heading>
+        <Text mb="2">
+          We use the information we collect in various ways, including to:
+        </Text>
+
+        <List styleType="disc" mb="2">
+          <ListItem>provide, operate, and maintain our website</ListItem>
+          <ListItem>improve, personalize, and expand our website</ListItem>
+          <ListItem>understand and analyze how you use our website</ListItem>
+          <ListItem>
+            develop new products, services, features, and functionality
+          </ListItem>
+          <ListItem>
+            communicate with you, either directly or through one of our
+            partners, including for customer service, to provide you with
+            updates and other information relating to the webste, and for
+            marketing and promotional purposes
+          </ListItem>
+          <ListItem>send you emails</ListItem>
+          <ListItem>find and prevent fraud</ListItem>
+        </List>
+        <Text mb="2">
+          We are allowed to transfer Personal Data collected within the EU to
+          third countries (i.e. any country not part of the EU) only pursuant to
+          a specific legal basis. Any such Data transfer is based on one of the
+          legal bases described below.
+        </Text>
+        <Text mb="2">
+          Users can inquire with NeedMask to learn which legal basis applies to
+          which specific service. Data transfer from the EU and/or Switzerland
+          to the U.S based on Privacy Shield (this Application)
+        </Text>
+        <Text mb="2">
+          If this is the legal basis, the transfer of Personal Data from the EU
+          or Switzerland to the US is carried out according to the EU - U.S. and
+          Swiss - U.S. Privacy Shield.
+        </Text>
+        <Text mb="2">
+          In particular, Personal Data is transferred to services that
+          self-certify under the Privacy Shield framework and therefore
+          guarantee an adequate level of protection of such transferred Data.
+          All services are listed within the relevant section of this document
+          and those that adhere to Privacy Shield can be singled out by checking
+          their privacy policy and possibly also by specifically checking for
+          Privacy Shield adherence in the official Privacy Shield List. Privacy
+          Shield also specifically guarantees rights to Users which can be found
+          in its most current form on the website run by the US Department of
+          Commerce.
+        </Text>
+        <Text mb="2">
+          Personal Data may be transferred from within the EU or Switzerland to
+          the U.S. to services that are not, or not anymore, part of Privacy
+          Shield, only based on other valid legal grounds. Users can ask the
+          Owner to learn about such legal grounds.
+        </Text>
+        <Text mb="2">
+          We work on servers provided by DigitalOcean, LLC. The servers are
+          based in Frankfurt, Germany. For more information about the Privacy
+          Shield generally, and to view DigitalOcean’s certification online,
+          please visit https://www.privacyshield.gov.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          Log Files
+        </Heading>
+        <Text mb="2">
+          NeedMask follows a standard procedure of using log files. These files
+          log visitors when they visit websites. All hosting companies do this
+          and a part of hosting services' analytics. The information collected
+          by log files include internet protocol (IP) addresses, browser type,
+          Internet Service Provider (ISP), date and time stamp, referring/exit
+          pages, and possibly the number of clicks. These are not linked to any
+          information that is personally identifiable. The purpose of the
+          information is for analyzing trends, administering the site, tracking
+          users' movement on the website, and gathering demographic information.
+          Our Privacy Policy was created with the help of the Privacy Policy
+          Generator and the Privacy Policy Template.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          Advertising Partners Privacy Policies
+        </Heading>
+        <Text mb="2">
+          You may consult this list to find the Privacy Policy for each of the
+          advertising partners of NeedMask.
+        </Text>
+        <Text mb="2">
+          Third-party ad servers or ad networks uses technologies like cookies,
+          JavaScript, or Web Beacons that are used in their respective
+          advertisements and links that appear on NeedMask, which are sent
+          directly to users' browser. They automatically receive your IP address
+          when this occurs. These technologies are used to measure the
+          effectiveness of their advertising campaigns and/or to personalize the
+          advertising content that you see on websites that you visit.
+        </Text>
+        <Text mb="2">
+          Note that NeedMask has no access to or control over these cookies that
+          are used by third-party advertisers.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          Third Party Privacy Policies
+        </Heading>
+        <Text mb="2">
+          NeedMask's Privacy Policy does not apply to other advertisers or
+          websites. Thus, we are advising you to consult the respective Privacy
+          Policies of these third-party ad servers for more detailed
+          information. It may include their practices and instructions about how
+          to opt-out of certain options. You may find a complete list of these
+          Privacy Policies and their links here: Privacy Policy Links.
+        </Text>
+        <Text mb="2">
+          You can choose to disable cookies through your individual browser
+          options. To know more detailed information about cookie management
+          with specific web browsers, it can be found at the browsers'
+          respective websites.
+        </Text>
+        <Text mb="2">
+          CCPA Privacy Rights (Do Not Sell My Personal Information)
+        </Text>
+        <Text mb="2">
+          Under the CCPA, among other rights, California consumers have the
+          right to:
+        </Text>
+        <Text mb="2">
+          Request that a business that collects a consumer's personal data
+          disclose the categories and specific pieces of personal data that a
+          business has collected about consumers.
+        </Text>
+        <Text mb="2">
+          Request that a business delete any personal data about the consumer
+          that a business has collected.
+        </Text>
+        <Text mb="2">
+          Request that a business that sells a consumer's personal data, not
+          sell the consumer's personal data.
+        </Text>
+        <Text mb="2">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          GDPR Data Protection Rights
+        </Heading>
+        <Text mb="2">
+          We would like to make sure you are fully aware of all of your data
+          protection rights. Every user is entitled to the following:
+        </Text>
+        <Text mb="2">
+          The right to access – You have the right to request copies of your
+          personal data. We may charge you a small fee for this service.
+        </Text>
+        <Text mb="2">
+          The right to rectification – You have the right to request that we
+          correct any information you believe is inaccurate. You also have the
+          right to request that we complete the information you believe is
+          incomplete.
+        </Text>
+        <Text mb="2">
+          The right to erasure – You have the right to request that we erase
+          your personal data, under certain conditions.
+        </Text>
+        <Text mb="2">
+          The right to restrict processing – You have the right to request that
+          we restrict the processing of your personal data, under certain
+          conditions.
+        </Text>
+        <Text mb="2">
+          The right to object to processing – You have the right to object to
+          our processing of your personal data, under certain conditions.
+        </Text>
+        <Text mb="2">
+          The right to data portability – You have the right to request that we
+          transfer the data that we have collected to another organization, or
+          directly to you, under certain conditions.
+        </Text>
+        <Text mb="2">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </Text>
+        <Heading as="h5" size="sm" my="2">
+          Children's Information
+        </Heading>
+        <Text mb="2">
+          Another part of our priority is adding protection for children while
+          using the internet. We encourage parents and guardians to observe,
+          participate in, and/or monitor and guide their online activity.
+        </Text>
+        <Text mb="2">
+          NeedMask does not knowingly collect any Personal Identifiable
+          Information from children under the age of 13. If you think that your
+          child provided this kind of information on our website, we strongly
+          encourage you to contact us immediately and we will do our best
+          efforts to promptly remove such information from our records.
+        </Text>
       </Box>
     </>
   );

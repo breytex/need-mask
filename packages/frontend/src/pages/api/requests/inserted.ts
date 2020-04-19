bc4d7ae270961b045adac28e276c813c065954e3
Firat Özcan <firat.oezcan@gmail.com>
2020-04-19 13:31:38 +0200

    Cannot request unpublished suppliers and changed favicon

--- packages/frontend/src/pages/api/requests/inserted.ts
+++ packages/frontend/src/pages/api/requests/inserted.ts
@@ -64,6 +64,9 @@ const handler = createWebhooookHandler<SupplierRequest>(async (req, res) => {
   if (!supplier) {
     return res.send("Supplier not found 😱");
   }
+  if (supplier.status !== 'published') {
+    return res.send("You cannot request from an unpublished supplier listing 💤")
+  }
 
   const subject = `${firstName} ${lastName} requested product information 📦`;
 
